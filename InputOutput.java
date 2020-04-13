c7d8a16229baba2a0b6c66bc95578135e31763cf
ssatriya1412 <ssatriya@gmail.com>
2020-04-13 15:54:07 +0700

    InputOutput

---
+++ InputOutput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InputOutput {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String nama;
+		int npm;
+		int jumlahSks;
+		double IPK;
+		
+		System.out.println("Masukkan nama Anda :");
+		nama = scanner.nextLine();
+		System.out.println("Hai " + nama);
+		
+		System.out.println("Masukkan NPM Anda :");
+		npm = scanner.nextInt();
+		System.out.println(npm);
+		
+		System.out.println("Masukkan jumlah SKS yang sudah ditempuh :");
+		jumlahSks = scanner.nextInt();
+		
+		System.out.println("Masukkan jumlah IPK sementara :");
+		IPK = scanner.nextDouble();
+		
+		double jumlah = IPK * jumlahSks;
+		System.out.println("Total Nilai = " + jumlah);
+	}
+}
--- model/InputOutput.java
+++
@@ -1,9 +0,0 @@
-public class InputOutput{
- public static void main(String [] args{
-  Scanner scanner = new Scanner(System.in);
-  String nama;
-  System.out.println("Masukkan nama Anda...");
-  nama = scanner.nextLine();
-  System.out.println("Hai +" nama);
- }
-}
